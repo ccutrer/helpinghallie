5fc71329255254b9972ca63e8fcc39aec4f139dc
Cody Cutrer <cody@instructure.com>
2019-03-30 22:17:24 -0600

    fix logging in heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,4 +59,14 @@ HelpingHallie::Application.configure do
   config.active_support.deprecation = :notify
 
   config.eager_load = true
+
+  # Disable serving static files from the `/public` folder by default since
+  # Apache or NGINX already handles this.
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
 end
